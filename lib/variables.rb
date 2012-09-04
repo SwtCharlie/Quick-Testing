ce73fd20a8c6e2c372f52ea65f5c9301abed388f
Tung Nguyen <tongueroo@gmail.com>
2012-09-04 11:47:18 -0700

    add more examples

--- lib/variables.rb
+++ lib/variables.rb
@@ -1,3 +1,10 @@
+# $ irb
+# > load 'lib/variables.rb'
+# > local = LocalVariableDemo.new
+# > local.tung
+
+$my_global_var = 'im seen everywhere'
+
 class LocalVariableDemo
   def tung
     local_var1 = 'tung'
@@ -10,6 +17,9 @@ class LocalVariableDemo
   def vuon2
     tung # i can only access it with a method
   end
+  def puts_global
+    puts $my_global_var
+  end
 end
 
 class InstanceVariableDemo
@@ -20,6 +30,55 @@ class InstanceVariableDemo
   def puts_instance_vars
     puts "@instance_var1 #{@instance_var1.inspect}"
     puts "@instance_var1 #{@instance_var1.inspect}"
+  end
+  def puts_global
+    puts $my_global_var
+  end
+end
+
+class InstanceVsClassMethodDemo
+  # called like this InstanceVsClassMethodDemo.new.instance_method
+  def instance_method
+    puts 'instance method called'
+  end
+  def puts_global
+    puts $my_global_var
+  end
+  # called like this InstanceVsClassMethodDemo.class_method
+  # note: you dont need a new instance
+  # you defined class methods with a self. in front
+  def self.class_method
+    puts 'instance method'
+  end
+  def self.puts_global
+    puts $my_global_var
+  end
+end
+
+# class variables are denoted with @@
+# instance varialables are denoted with @
+class ClassVariableDemo
+  # instance method definitions follows
+  def set_instance_var
+    @instance_var = 'instance var tung'
+  end
+  def puts_instance_var
+    puts "@instance_var #{@instance_var.inspect}"
+  end
+  # instance variables have access to @@class_var
+  def instance_method_that_puts_class_var
+    puts "@@class_var #{@@class_var.inspect}"
+  end
+  # class method definitions follows
+  def self.set_class_var
+    @@class_var = 'instance var tung'
+  end
+  def self.puts_class_var
+    puts "@@class_var #{@@class_var.inspect}"
+  end
+  # instance variables have access to @@class_var
+  def self.class_method_that_puts_class_var
+    puts "@@class_var #{@@class_var.inspect}"
   end
 end
 
