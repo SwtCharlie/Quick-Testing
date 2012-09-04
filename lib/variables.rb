987a275e2ad96f96e4d9f7ba78e82ec42063b9de
Tung Nguyen <tongueroo@gmail.com>
2012-09-04 12:34:32 -0700

    add comments

--- lib/variables.rb
+++ lib/variables.rb
@@ -1,10 +1,10 @@
-# $ irb
+$my_global_var = 'im seen everywhere'
+
 # > load 'lib/variables.rb'
 # > local = LocalVariableDemo.new
 # > local.tung
-
-$my_global_var = 'im seen everywhere'
-
+# > local.vuon
+# > local.vuon2
 class LocalVariableDemo
   def tung
     local_var1 = 'tung'
@@ -25,6 +25,11 @@ class LocalVariableDemo
   end
 end
 
+# > load 'lib/variables.rb'
+# > demo = InstanceVariableDemo.new
+# > demo.puts_instance_vars
+# > demo.set_instance_vars
+# > demo.puts_instance_vars
 class InstanceVariableDemo
   def set_instance_vars
     @instance_var1 = 'tung1'
@@ -39,6 +44,12 @@ class InstanceVariableDemo
   end
 end
 
+# > load 'lib/variables.rb'
+# > demo = InstanceVsClassMethodDemo.new
+# > demo.instance_method
+# > demo.class_method # will error, this is a class not instancemethod
+# > InstanceVsClassMethodDemo.class_method
+# > InstanceVsClassMethodDemo.class_method # will error, this is a instance not class method
 class InstanceVsClassMethodDemo
   # called like this InstanceVsClassMethodDemo.new.instance_method
   def instance_method
@@ -60,6 +71,12 @@ end
 
 # class variables are denoted with @@
 # instance varialables are denoted with @
+# 
+# > ClassVariableDemo.puts_class_var
+# > ClassVariableDemo.set_class_var
+# > ClassVariableDemo.puts_class_var
+# > demo = ClassVariableDemo.new
+# > demo.instance_method_that_puts_class_var
 class ClassVariableDemo
   # instance method definitions follows
   def set_instance_var
@@ -73,14 +90,10 @@ class ClassVariableDemo
     puts "@@class_var #{@@class_var.inspect}"
   end
   # class method definitions follows
-  def self.set_class_var
-    @@class_var = 'instance var tung'
+  def self.set_class_var(val='default')
+    @@class_var = "instance var #{val}"
   end
   def self.puts_class_var
-    puts "@@class_var #{@@class_var.inspect}"
-  end
-  # instance variables have access to @@class_var
-  def self.class_method_that_puts_class_var
     puts "@@class_var #{@@class_var.inspect}"
   end
 end
